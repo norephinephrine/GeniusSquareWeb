9120305d634d5d5bb1e6640d48cc2af78e6c1480
Jovan <test@gmail.com>
2024-08-14 19:27:02 +0200

    Renaming of project

---
+++ src/Models/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using GeniusSquareWeb.Models;
+using System.Collections.Concurrent;
+
+namespace GeniusSquareWeb.Server
+{
+    /// <summary>
+    /// Class that manages game instances of Genius Square.
+    /// </summary>
+    public class GameManager: IGameManager
+    {
+
+        private IEnumerable<IDice> dices;
+        private static readonly ConcurrentDictionary<Guid, GameInstance> gameInstances = new();
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public GameManager(IEnumerable<IDice> dices)
+        {
+            this.dices = dices;
+        }
+
+        /// <inheritdoc/>
+        public GameInstance? TryCreateGame()
+        {
+            Guid gameId = Guid.NewGuid();
+            GameBoard board = new GameBoard();
+
+            foreach (IDice dice in dices)
+            {
+                GameBoardField field = dice.GenerateDiceResult();
+                board.SetGameBoardField(field, -1);
+            }
+
+            GameInstance gameInstance = new(gameId, board);
+
+            if (!gameInstances.TryAdd(gameId, gameInstance))
+            {
+                return null;
+            }
+
+            return gameInstance;
+        }
+
+        /// <inheritdoc/>
+        public GameInstance? TryGetExistingGame(Guid gameId)
+        {
+            GameInstance gameInstance;
+            if (!gameInstances.TryGetValue(gameId, out gameInstance))
+            {
+                return null;
+            }
+
+            return gameInstance;
+        }
+
+        /// <inheritdoc/>
+        public bool TryDeleteGame(Guid gameId)
+        {
+            return gameInstances.TryRemove(gameId, out GameInstance gameBoard);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<IGameInstance> GetAllGameInstances()
+        {
+            return gameInstances.Values;
+        }
+    }
+}
